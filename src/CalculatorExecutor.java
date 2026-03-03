473174558cf52a70d807a642dcd381ba6a7dfe61
HanByeol Yang <beuri97@naver.com>
2026-03-03 15:55:19 +0900

    feat: create class CalculatorExecutor

---
+++ src/CalculatorExecutor.java
@@ -0,0 +1,18 @@
+public class CalculatorExecutor {
+
+    private CommandLineInterface cli;
+
+    public CalculatorExecutor() {
+        this.cli = new CommandLineInterface();
+    }
+
+    public void play() {
+        while (true) {
+
+            // TODO: Implement
+            int lhsOperand = cli.returnOperand("첫 번째 피연산자를 입력하세요");
+            String operator = cli.returnOperator("연산자를 입력하세요 (+, -, *, /)");
+            int rhsOperand = cli.returnOperand("두 번째 피연산자를 입력하세요");
+        }
+    }
+}
