e28652f23783ba7241e5d812d6556f64f1fa6779
HanByeol Yang <beuri97@naver.com>
2026-03-03 18:04:53 +0900

    feat: apply class CalculatorHistory and handle exceptions

--- src/CalculatorExecutor.java
+++ src/CalculatorExecutor.java
@@ -10,9 +10,29 @@ public class CalculatorExecutor {
         while (true) {
 
             // TODO: Implement
-            int lhsOperand = cli.returnOperand("첫 번째 피연산자를 입력하세요");
-            String operator = cli.returnOperator("연산자를 입력하세요 (+, -, *, /)");
-            int rhsOperand = cli.returnOperand("두 번째 피연산자를 입력하세요");
+            String lhsOperand = cli.returnValue("첫 번째 피연산자를 입력하세요");
+            String operator = cli.returnValue("연산자를 입력하세요 (+, -, *, /)");
+            String rhsOperand = cli.returnValue("두 번째 피연산자를 입력하세요");
+            CalculatorHistory history;
+
+            try {
+                history = new CalculatorHistory(Double.parseDouble(lhsOperand), Double.parseDouble(rhsOperand), CalculatorOperator.fromSymbol(operator)).calculate();
+
+            } catch (NumberFormatException e){
+                System.out.println("유효하지 않은 숫자입니다. 다시 입력해주세요.");
+                continue;
+            } catch (IllegalArgumentException e) {
+                System.out.println("유효하지 않은 연산자입니다. 다시 입력해주세요.");
+                continue;
+            } catch (ArithmeticException e) {
+                System.out.println("0으로 나눌 수 없습니다.");
+                continue;
+            }
+
+            System.out.println(history);
+
+
+
         }
     }
 }
